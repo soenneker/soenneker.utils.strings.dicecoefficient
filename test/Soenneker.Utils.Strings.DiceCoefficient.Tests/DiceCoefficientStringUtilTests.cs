001086293a2643df20f095bac497f29aae0d2ef6
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-22 19:51:05 -0500

    Various fixes

--- test/Soenneker.Utils.Strings.DiceCoefficient.Tests/DiceCoefficientStringUtilTests.cs
+++ test/Soenneker.Utils.Strings.DiceCoefficient.Tests/DiceCoefficientStringUtilTests.cs
@@ -11,18 +11,18 @@ public class DiceCoefficientStringUtilTests : HostedUnitTest
     {
     }
 
-    [Theory]
-    [InlineData("", "", 1)] // Two empty strings are identical
-    [InlineData("abc", "", 0)] // One empty string results in no similarity
-    [InlineData("", "xyz", 0)] // One empty string results in no similarity
-    [InlineData("kitten", "sitting", 0.3636)] // Calculated Dice Coefficient
-    [InlineData("kitten", "kitten", 1)] // Identical strings have full similarity
-    [InlineData("abc", "def", 0)] // Completely different strings, no shared bigrams
-    [InlineData("abcdef", "abc", 0.5714)] // Calculated Dice Coefficient
-    [InlineData("abc", "abcd", 0.8)] // Calculated Dice Coefficient
-    [InlineData("this is sitting on a porch", "this is sitting", 0.717)] // Calculated similarity
-    [InlineData("the cat sat on a hat", "sad on a hat", 0.6)] // Calculated similarity
-    [InlineData("this is a test", "this is another test", 0.75)] // Calculated similarity
+    [Test]
+    [Arguments("", "", 1)] // Two empty strings are identical
+    [Arguments("abc", "", 0)] // One empty string results in no similarity
+    [Arguments("", "xyz", 0)] // One empty string results in no similarity
+    [Arguments("kitten", "sitting", 0.3636)] // Calculated Dice Coefficient
+    [Arguments("kitten", "kitten", 1)] // Identical strings have full similarity
+    [Arguments("abc", "def", 0)] // Completely different strings, no shared bigrams
+    [Arguments("abcdef", "abc", 0.5714)] // Calculated Dice Coefficient
+    [Arguments("abc", "abcd", 0.8)] // Calculated Dice Coefficient
+    [Arguments("this is sitting on a porch", "this is sitting", 0.717)] // Calculated similarity
+    [Arguments("the cat sat on a hat", "sad on a hat", 0.6)] // Calculated similarity
+    [Arguments("this is a test", "this is another test", 0.75)] // Calculated similarity
 
     public void Calculate_Returns_Correct_Similarity_Score(string str1, string str2, double expectedScore)
     {
